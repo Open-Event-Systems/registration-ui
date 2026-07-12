91aafff258b973e67b01cda499d2c2b9f02a3877
Wes <wes@anthronewengland.com>
2026-07-11 23:09:25 -0400

    Add style file module declarations

--- registration/common/typings.d.ts
+++ registration/common/typings.d.ts
@@ -1,3 +1,11 @@
+declare module "*.scss" {
+  const value: string
+  export default value
+}
+declare module "*.css" {
+  const value: string
+  export default value
+}
 declare module "*.svg" {
   const value: string
   export default value
